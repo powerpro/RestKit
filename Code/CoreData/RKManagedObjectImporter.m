2df01277938c1a5094bb8a5286bfcab828d6ea3f
Tim Ritchey <tritchey@redromelogic.com>
2019-05-01 08:33:04 -0400

    change concurrency to 1 to fix crashers

--- Code/CoreData/RKManagedObjectImporter.m
+++ Code/CoreData/RKManagedObjectImporter.m
@@ -269,7 +269,7 @@
 {
     // Perform our connection operations in a batch, before we save the MOC
     RKLogInfo(@"Starting %lu connection operations...", (unsigned long) self.connectionQueue.operationCount);
-    [self.connectionQueue setMaxConcurrentOperationCount:50];
+    [self.connectionQueue setMaxConcurrentOperationCount:1];
     [self.connectionQueue setSuspended:NO];
     [self.connectionQueue waitUntilAllOperationsAreFinished];
 
